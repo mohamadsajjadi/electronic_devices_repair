7e09a7904df6b8a78fe569cd231fbd6974a6c580
Yashar1989 <yashar1989@gmail.com>
2024-02-20 20:00:55 +0330

    add Category and Rate models to Projects app

--- core/projects/models.py
+++ core/projects/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+
+class Category(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255)
+    parent = models.ForeignKey(to="self", on_delete=models.PROTECT, null=True, blank=True)
+    description = models.TextField()
+
+
+class Rate(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    # project = models.ForeignKey() ForeignKey to Project table
+    rate_score = models.PositiveSmallIntegerField()
+    comment = models.TextField()
